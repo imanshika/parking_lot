40fa4b2ca939eb18fff0fcc15d78daac76aff227
Anshika Jain <anshikajain2107@gmail.com>
2026-03-14 22:20:10 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,6 @@ public class Main {
     }
 
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
         //Create parking lot
         ParkingLot parkingLot = new ParkingLot(10, 10);
